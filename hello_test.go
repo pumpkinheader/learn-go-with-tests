56fd380e5532d7d708f64841825b6ce00c426433
root <6647001+pumpkinheader@users.noreply.github.com>
2023-05-02 09:48:53 +0000

    first argument: name

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("mikajui")
-	want := "Hello, mikajui"
-
-	if	got != want {
-		t.Errorf("got %q want %q", got , want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("say 'HEllo, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("mikajui")
+		want := "Hello, mikajui"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
