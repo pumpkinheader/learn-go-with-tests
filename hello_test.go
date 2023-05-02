9f52f604d364636ee120c89444aa2c5dd7d98742
root <6647001+pumpkinheader@users.noreply.github.com>
2023-05-02 11:09:59 +0000

    second argument: language

--- hello_test.go
+++ hello_test.go
@@ -10,14 +10,26 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("say 'HEllo, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("mikajui")
+		got := Hello("mikajui", "")
 		want := "Hello, mikajui"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got:= Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got:= Hello("Hiroyuki", "French")
+		want := "Bonjour, Hiroyuki"
 		assertCorrectMessage(t, got, want)
 	})
 }
