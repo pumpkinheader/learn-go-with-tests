fc4c887f99fd377545260338e2d6aeeb65d0212f
pumpkinheader <6647001+pumpkinheader@users.noreply.github.com>
2023-05-04 03:48:22 +0000

    [iteration] コールを間違えないように例を追加

--- iteration/hasprefix_test.go
+++ iteration/hasprefix_test.go
@@ -2,6 +2,7 @@ package iteration // Package名はいったん気にしないことにする
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestHasPrefix (t *testing.T) {
@@ -11,4 +12,12 @@ func TestHasPrefix (t *testing.T) {
 	if got != want {
 		t.Errorf("expected %t but got %t", want, got)
 	}
+}
+
+func ExampleHasPrefix() {
+	fmt.Println(HasPrefix("＼(・ω・＼)SAN値!(／・ω・)／ピンチ!", "＼(・ω・＼)"))
+	fmt.Println(HasPrefix("＼(・ω・＼)SAN値!(／・ω・)／ピンチ!", "(」・ω・)」"))
+	// Output:
+	// true
+	// false
 }
