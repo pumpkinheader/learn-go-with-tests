316d32ff2a34af9c32938184b9194ee56b5dd5e6
pumpkinheader <6647001+pumpkinheader@users.noreply.github.com>
2023-05-04 03:38:07 +0000

    [iteration] 変数宣言が無駄なので、削除

--- iteration/hasprefix.go
+++ iteration/hasprefix.go
@@ -1,6 +1,5 @@
 package iteration // Package名はいったん気にしないことにする
 
 func HasPrefix (text string, prefix string) bool {
-	split := len(prefix)
-	return text[0:split] == prefix
+	return text[0:len(prefix)] == prefix
 }
