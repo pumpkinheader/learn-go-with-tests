a5210aa499ffd7a2ffad6b1358eec0d8476af9dd
pumpkinheader <6647001+pumpkinheader@users.noreply.github.com>
2023-05-05 12:36:18 +0000

    [structs] 要件追加のためロジックを追加

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(20.0, 30.0)
+	want := 600.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
