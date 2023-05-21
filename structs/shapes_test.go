4d3c22f8fe80947862306dc38b0534ae5cdc6608
pumpkinheader <6647001+pumpkinheader@users.noreply.github.com>
2023-05-21 04:02:17 +0000

    [structs] スライスからテストケースの詳細を把握できるようにする

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -16,17 +16,17 @@ func TestArea(t *testing.T) {
 	areaTests := []struct {
 		name string
 		shape Shape
-		want float64
+		hasArea float64
 	}{
-		{name: "Rectangle", shape: Rectangle{20.0, 30.0}, want: 600},
-		{name: "Circle", shape: Circle{10.0}, want: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{20.0, 30.0}, want: 300.0},
+		{name: "Rectangle", shape: Rectangle{Height:20.0, Width:30.0}, hasArea: 600},
+		{name: "Circle", shape: Circle{Radius:10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base:20.0, Height:30.0}, hasArea: 300.0},
 	}
 	for _, tt := range areaTests{
 		t.Run(tt.name, func(t *testing.T){
 			got := tt.shape.Area()
-			if got != tt.want {
-				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 			}
 		})
 	}
